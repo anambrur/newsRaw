e96aae9205e693fc9fdad79eaeea49c55dbf5df9
anambrur <anambrur@gmail.com.com>
2025-05-25 15:49:55 +0600

    added static reporter name5

--- admin/add-news.php
+++ admin/add-news.php
@@ -205,7 +205,7 @@ if ($_SERVER['REQUEST_METHOD'] === 'POST' && (isset($_POST['submit']) || isset($
 
                     mysqli_stmt_bind_param(
                         $insertQuery,
-                        'sisssiiiiisisssssssss',
+                        'sisssiiiiisissssssssss',
                         $posttitle,
                         $catid,
                         $postdetails,
