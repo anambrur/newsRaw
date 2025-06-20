a21d98f14ba2fefd3c3969a01c9ff5fcfd7820b9
anambrur <anambrur@gmail.com.com>
2025-06-20 22:31:08 +0600

    image edit 7

--- admin/z-change-news-image.php
+++ admin/z-change-news-image.php
@@ -3,7 +3,7 @@ session_start();
 include('includes/config.php');
 include('includes/resizeLib.php');
 error_reporting(E_ALL);
-ini_set('display_errors', 1);
+ini_set('display_errors', 0);
 
 function imageGenerator($input, $overlay, $output)
 {
